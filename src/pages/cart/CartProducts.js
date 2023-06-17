035184a0d06010453b34c3579aae610769e56f56
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-17 16:31:17 +0100

    update CartProducts.js

--- src/pages/cart/CartProducts.js
+++ src/pages/cart/CartProducts.js
@@ -5,21 +5,17 @@ import CartItem from './CartItem';
 import styles from './CartProducts.module.scss';
 
 const CartProducts = () => {
+  const { cartItems } = useContext(CartContext);
 
-    const { cartItems } = useContext(CartContext);
-
-    return (
-        <div className={styles.p__container}>
-            <div className="card card-body border-0">
-
-                {
-                    cartItems.map(product =>  <CartItem key={product.id} product={product}/>)
-                }
-
-            </div>
-        </div>
-
-     );
-}
+  return (
+    <div className={styles.p__container}>
+      <div className="card card-body border-0">
+        {cartItems.map((product) => (
+          <CartItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CartProducts;
